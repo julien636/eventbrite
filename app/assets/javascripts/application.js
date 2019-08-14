dbce3a03cc1d9ac947e746de9ef476308db803e0
Julien <julien63650@gmail.com>
2019-08-14 03:43:47 +0200

    debug js 4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
 //= require jquery
 //= require jquery_ujs
+//= require_tree .
+//= require activestorage
+//= require turbolinks
+
